9dbbf617235374628bad9edac35ca540223b0a4c
Sanika Mahamulkar <sanikamahamulkar13@gmail.com>
2026-09-21 21:39:31 +0100

    Add least-privilege IAM policy scoped from Access Advisor evidence

---
+++ iam-least-privilege.tf
@@ -0,0 +1,60 @@
+data "aws_iam_policy_document" "terraform_scoped" {
+  statement {
+    sid    = "CoreInfrastructure"
+    effect = "Allow"
+    actions = [
+      "ec2:*",
+      "s3:*",
+      "dynamodb:*",
+      "cloudtrail:*",
+      "guardduty:*",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "IAMManagement"
+    effect = "Allow"
+    actions = [
+      "iam:GetUser",
+      "iam:GetRole",
+      "iam:GetPolicy",
+      "iam:GetPolicyVersion",
+      "iam:ListPolicies",
+      "iam:ListAttachedUserPolicies",
+      "iam:ListAttachedRolePolicies",
+      "iam:CreatePolicy",
+      "iam:CreatePolicyVersion",
+      "iam:DeletePolicy",
+      "iam:DeletePolicyVersion",
+      "iam:AttachUserPolicy",
+      "iam:DetachUserPolicy",
+      "iam:AttachRolePolicy",
+      "iam:DetachRolePolicy",
+      "iam:CreateRole",
+      "iam:DeleteRole",
+      "iam:PassRole",
+      "iam:TagRole",
+      "iam:TagPolicy",
+      "iam:CreateServiceLinkedRole",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "IdentityAndAccountRead"
+    effect = "Allow"
+    actions = [
+      "sts:GetCallerIdentity",
+      "organizations:DescribeOrganization",
+      "access-analyzer:*",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "terraform_scoped" {
+  name        = "security-lab-terraform-scoped-policy"
+  description = "Least-privilege policy for this project, scoped from Access Advisor evidence"
+  policy      = data.aws_iam_policy_document.terraform_scoped.json
+}
