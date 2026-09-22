cc567e212b36b59b610a3a032e4100d36f8378cc
Sanika Mahamulkar <sanikamahamulkar13@gmail.com>
2026-09-22 21:20:31 +0100

    Fix SSM registration: install amazon-ssm-agent via user_data (AMI does not ship it pre-installed) - confirmed working via remote command execution

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -28,6 +28,7 @@ resource "aws_instance" "target" {
   monitoring              = true
   user_data              = <<-USERDATA
     #!/bin/bash
+    dnf install -y https://s3.eu-west-2.amazonaws.com/amazon-ssm-eu-west-2/latest/linux_amd64/amazon-ssm-agent.rpm
     systemctl enable amazon-ssm-agent
     systemctl start amazon-ssm-agent
     USERDATA
